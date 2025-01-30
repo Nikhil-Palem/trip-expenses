e07ec1e72f83cdfeee33d596e0db45e479c6f61e
nikhil <nikhilpalem93466@gmail.com>
2025-01-31 00:17:41 +0530

    domain change

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Configure CORS
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://trip-expenses-website.vercel.app'],
+    origin: ['http://localhost:5173', 'https://trip-expenses.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
